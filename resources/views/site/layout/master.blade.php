33b18f00708d0a3114d2459fed18c725b7116507
Lucas Olmedo Silva <lsilva@meeta.com.br>
2019-12-30 17:19:27 -0300

    correção CSS sem carregar no HTTPS

--- resources/views/site/layout/master.blade.php
+++ resources/views/site/layout/master.blade.php
@@ -2,28 +2,31 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+
     <title>@yield('title')</title>
+
     <meta name="viewport" content="width=device-width; initial-scale=1.0;">
     <meta content="" name="keywords">
     <meta content="" name="description">
+
     <!-- Favicons -->
-    <link href="{{ secure_asset('img/favicon.png') }}" rel="icon">
-    <link href="{{ secure_asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+    <link href="/img/favicon.png" rel="icon">
+    <link href="/img/apple-touch-icon.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
-    <link href="{{ secure_asset('css/font.css')}}" rel="stylesheet">
+    <link href="/css/font.css" rel="stylesheet">
 
     <!-- Bootstrap CSS File -->
-    <link href="{{ secure_asset('lib/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Libraries CSS Files -->
-    <link href="{{ secure_asset('lib/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('lib/animate/animate.min.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('lib/venobox/venobox.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="/lib/venobox/venobox.css" rel="stylesheet">
+    <link href="/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 
     <!-- Main Stylesheet File -->
-    <link href="{{ secure_asset('css/style.css') }}" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
 
     <!-- Custom CSS Files -->
     @stack('page-css')
@@ -43,23 +46,23 @@
 <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
 
 <!-- Essential JS -->
-<script src="{{ secure_asset('lib/jquery/jquery.min.js') }}"></script>
-<script src="{{ secure_asset('lib/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="/lib/jquery/jquery.min.js"></script>
+<script src="/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- JavaScript Libraries -->
-<script src="{{ secure_asset('lib/jquery/jquery-migrate.min.js') }}"></script>
-<script src="{{ secure_asset('lib/easing/easing.min.js') }}"></script>
-<script src="{{ secure_asset('lib/superfish/hoverIntent.js') }}"></script>
-<script src="{{ secure_asset('lib/superfish/superfish.min.js') }}"></script>
-<script src="{{ secure_asset('lib/wow/wow.min.js') }}"></script>
-<script src="{{ secure_asset('lib/venobox/venobox.min.js') }}"></script>
-<script src="{{ secure_asset('lib/owlcarousel/owl.carousel.min.js')}}"></script>
+<script src="/lib/jquery/jquery-migrate.min.js"></script>
+<script src="/lib/easing/easing.min.js"></script>
+<script src="/lib/superfish/hoverIntent.js"></script>
+<script src="/lib/superfish/superfish.min.js"></script>
+<script src="/lib/wow/wow.min.js"></script>
+<script src="/lib/venobox/venobox.min.js"></script>
+<script src="/lib/owlcarousel/owl.carousel.min.js"></script>
 
 <!-- Contact Form JavaScript File -->
-<script src="{{ secure_asset('js/contactform.js') }}"></script>
+<script src="/js/contactform.js"></script>
 
 <!-- Template Main Javascript File -->
-<script src="{{ secure_asset('js/main.js') }}"></script>
+<script src="/js/main.js"></script>
 
 <!-- Custom JS Files -->
 @stack('page-js')
