bd68ec0817d975cf2e3f3095b22c807e791c4e59
Lucas Olmedo Silva <lsilva@meeta.com.br>
2020-07-13 20:40:13 -0300

    corrẽção igrejas filhas

--- resources/views/site/layout/partials/about.blade.php
+++ resources/views/site/layout/partials/about.blade.php
@@ -4,9 +4,9 @@
             <div class="col-lg-6">
                 <h2>Sobre nós</h2>
                 <p>A Igreja Batista Nova Vida foi fundada em 12 de setembro de 1992. É uma Igreja de origem batista e de
-                    confissão pentecostal. Desde seu inicio foi pastoreada pelo Pr. Isac de Souza e tem sua sede em São
+                    confissão pentecostal. Desde seu inicio foi pastoreada pelo Bispo Isac de Souza e tem sua sede em São
                     Paulo capital.</p>
-                <p>Atualmente a Igreja conta além da sede com mais 12 Igrejas filhas e está sendo abençoada pelo Senhor
+                <p>Atualmente a Igreja conta além da sede com mais 5 Igrejas filhas e está sendo abençoada pelo Senhor
                     com crescimento. A Igreja Batista Nova Vida tem desenvolvido intenso ministério de evangelismo
                     através de cruzadas evangelísticas de massa e de programa diário de rádio.</p>
                 <p>Em 2016, além de dar extensão aos ministérios de alcance evangelístico, a Igreja Batista Nova Vida
