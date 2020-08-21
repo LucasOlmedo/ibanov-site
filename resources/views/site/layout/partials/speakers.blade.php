4e5db35be24316d47bc2648bd0810c69e34b313f
Lucas Olmedo Silva <lsilva@meeta.com.br>
2020-08-21 17:12:25 -0300

    modal biografia

--- resources/views/site/layout/partials/speakers.blade.php
+++ resources/views/site/layout/partials/speakers.blade.php
@@ -15,7 +15,9 @@
                         <h3>Bispo Isac de Souza</h3>
                         <br>
                         <div class="social">
-                            <a href=""><i class="fa fa-file-text"></i> Biografia</a>
+                            <a href="#modal-bio-1" data-toggle="modal" data-target="#modal-bio-1">
+                                <i class="fa fa-file-text"></i> Biografia
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -27,7 +29,9 @@
                         <h3>Pastor Jaime Nascimento</h3>
                         <br>
                         <div class="social">
-                            <a href=""><i class="fa fa-file-text"></i> Biografia</a>
+                            <a href="#modal-bio-2" data-toggle="modal" data-target="#modal-bio-2">
+                                <i class="fa fa-file-text"></i> Biografia
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -35,3 +39,65 @@
         </div>
     </div>
 </section>
+
+<!-- MODAL -->
+<div class="modal fade modal-custom-site" id="modal-bio-1" aria-labelledby="modal-bio-title">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-bio-title">Biografia - Bispo Isac de Souza</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias aliquam dicta dignissimos dolorum
+                ducimus enim eveniet optio provident quas. Ab accusantium aliquam facilis, natus optio qui recusandae
+                reprehenderit sapiente.
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dolore eum exercitationem harum ipsa
+                omnis optio qui quis totam veritatis! Amet aperiam eveniet ipsum labore modi necessitatibus nesciunt
+                odio, odit!
+                <br>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate deleniti dicta laudantium, placeat
+                praesentium ut velit! Commodi consectetur modi natus nemo officiis qui sunt velit? Cupiditate doloribus
+                illo possimus sint.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet aperiam distinctio esse excepturi,
+                incidunt iste labore laudantium mollitia nisi officiis velit voluptatem voluptatum? Debitis eos fugiat
+                maiores quis repellendus!
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- MODAL -->
+<div class="modal fade modal-custom-site" id="modal-bio-2" aria-labelledby="modal-bio-title-2">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-bio-title-2">Biografia - Pastor Jaime Nascimento</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias aliquam dicta dignissimos dolorum
+                ducimus enim eveniet optio provident quas. Ab accusantium aliquam facilis, natus optio qui recusandae
+                reprehenderit sapiente.
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dolore eum exercitationem harum ipsa
+                omnis optio qui quis totam veritatis! Amet aperiam eveniet ipsum labore modi necessitatibus nesciunt
+                odio, odit!
+                <br>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate deleniti dicta laudantium, placeat
+                praesentium ut velit! Commodi consectetur modi natus nemo officiis qui sunt velit? Cupiditate doloribus
+                illo possimus sint.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet aperiam distinctio esse excepturi,
+                incidunt iste labore laudantium mollitia nisi officiis velit voluptatem voluptatum? Debitis eos fugiat
+                maiores quis repellendus!
+            </div>
+        </div>
+    </div>
+</div>
